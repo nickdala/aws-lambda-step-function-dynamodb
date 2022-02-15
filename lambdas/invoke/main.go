e27000688e4500cb65b815ee200d97b13fedd6c0
Nick Dalalelis <nick@nickthecloudguy.com>
2022-02-15 11:14:32 -0500

    Initial commit

---
+++ lambdas/invoke/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/sfn"
+)
+
+type MyEvent struct {
+	TaskId string `json:"taskId"`
+}
+
+type StepFunctionInput struct {
+	TaskId    string `json:"taskId"`
+	Timestamp int64  `json:"timestamp"`
+}
+
+func HandleRequest(ctx context.Context, myEvent MyEvent) (string, error) {
+	taskId := myEvent.TaskId
+	log.Printf("Task ID: %s", taskId)
+
+	stateMachineArn := os.Getenv("StateMachineArn")
+	log.Printf("State Machine Arm: %s", stateMachineArn)
+
+	unixTime := time.Now().Unix()
+
+	initialState := &StepFunctionInput{
+		TaskId:    taskId,
+		Timestamp: unixTime,
+	}
+
+	initialStateAsBytes, _ := json.Marshal(initialState)
+	initialStateAsString := string(initialStateAsBytes)
+	log.Printf("This is initial state %s\n", initialStateAsString)
+
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		panic("unable to load SDK config, " + err.Error())
+	}
+
+	client := sfn.NewFromConfig(cfg)
+
+	stateMachineExcutionName := fmt.Sprintf("%s-%v", taskId, unixTime)
+	input := &sfn.StartExecutionInput{
+		StateMachineArn: &stateMachineArn,
+		Input:           &initialStateAsString,
+		Name:            &stateMachineExcutionName,
+	}
+
+	result, err := client.StartExecution(ctx, input)
+	if err != nil {
+		log.Printf("StartExecution error: %v\n", err)
+		return "", err
+	} else {
+		log.Printf("Started step function executed: %v\n", result)
+	}
+
+	return fmt.Sprintf("Started step function execution id %s!", stateMachineExcutionName), nil
+}
+
+func main() {
+	lambda.Start(HandleRequest)
+}
--- lib/aws-stepfunction-status-stack.ts
+++ lib/aws-stepfunction-status-stack.ts
@@ -1,16 +1,89 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Stack, StackProps, RemovalPolicy, Duration } from 'aws-cdk-lib';
+import { AttributeType, Table, BillingMode } from 'aws-cdk-lib/aws-dynamodb';
+import * as sfn from 'aws-cdk-lib/aws-stepfunctions';
+import * as tasks from 'aws-cdk-lib/aws-stepfunctions-tasks';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambdago from '@aws-cdk/aws-lambda-go-alpha';
 
 export class AwsStepfunctionStatusStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    /* Tasks DynamoDB table */
+    const dynamoTable = new Table(this, 'Tasks', {
+      partitionKey: {
+        name: 'taskId',
+        type: AttributeType.STRING
+      },
+      sortKey: {
+        name: 'timestamp',
+        type: AttributeType.NUMBER
+      },
+      tableName: 'Tasks',
+      billingMode: BillingMode.PAY_PER_REQUEST,
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsStepfunctionStatusQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+      /**
+       *  The default removal policy is RETAIN, which means that cdk destroy will not attempt to delete
+       * the new table, and it will remain in your account until manually deleted. By setting the policy to
+       * DESTROY, cdk destroy will delete the table (even if it has data in it)
+       */
+      removalPolicy: RemovalPolicy.DESTROY, // NOT recommended for production code
+    });
+
+    /** ------------------ Step functions Definition ------------------ */
+    const logStartTask = new tasks.DynamoPutItem(this, 'CreateDynamoTaskItem', {
+      item: {
+        taskId: tasks.DynamoAttributeValue.fromString(sfn.JsonPath.stringAt('$.taskId')),
+        //timestamp: tasks.DynamoAttributeValue.numberFromString(sfn.JsonPath.stringAt('$.timestamp')),
+        // Can't use fromNumber - see https://github.com/aws/aws-cdk/issues/12456
+        // timestamp: tasks.DynamoAttributeValue.fromNumber(sfn.JsonPath.numberAt('$.timestamp')),
+        timestamp: tasks.DynamoAttributeValue.numberFromString(
+          sfn.JsonPath.stringAt(`States.Format('{}', ${'$.timestamp'})`)),
+        Status: tasks.DynamoAttributeValue.fromString("STARTED")
+      },
+      table: dynamoTable,
+      
+      resultPath: sfn.JsonPath.DISCARD
+    });
+
+    const waitX = new sfn.Wait(this, 'Wait X Seconds', {
+      time: sfn.WaitTime.duration(Duration.seconds(30)),
+    });
+
+    const logEndTask = new tasks.DynamoUpdateItem(this, 'UpdateDynamoTaskItem', {
+      key: {
+        taskId: tasks.DynamoAttributeValue.fromString(sfn.JsonPath.stringAt('$.taskId')),
+        //timestamp: tasks.DynamoAttributeValue.numberFromString(sfn.JsonPath.stringAt('$.timestamp')),
+        // Can't use fromNumber - see https://github.com/aws/aws-cdk/issues/12456
+        // timestamp: tasks.DynamoAttributeValue.fromNumber(sfn.JsonPath.numberAt('$.timestamp')),
+        timestamp: tasks.DynamoAttributeValue.numberFromString(
+          sfn.JsonPath.stringAt(`States.Format('{}', ${'$.timestamp'})`))
+      },
+      table: dynamoTable,
+      expressionAttributeValues: {
+        ':val': tasks.DynamoAttributeValue.fromString('Done')
+      },
+      expressionAttributeNames: {
+        "#s": "Status"
+      },
+      updateExpression: 'SET #s = :val',
+    });
+
+    const definition =  logStartTask.next(waitX).next(logEndTask);
+    const stateMachine = new sfn.StateMachine(this, 'StateMachine', {
+      definition
+    });
+    // don't forget permissions. You need to assign them
+    dynamoTable.grantWriteData(stateMachine);
+
+    // The code that defines your stack goes here
+    const invokeStepFunction = new lambdago.GoFunction(this, 'invokeStepFunction', {
+      entry: 'lambdas/invoke',
+      environment: {
+        StateMachineArn: stateMachine.stateMachineArn
+      },
+    });
+
+    stateMachine.grantStartExecution(invokeStepFunction)
   }
 }
